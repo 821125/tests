1488b8481f714051e008fe9b805686f355ae4813
Andreas Dorst <dorst.andreas@gmail.com>
2022-09-18 09:02:02 +0300

    task 2 is done

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,11 +1,6 @@
 from capitalize import capitalize
 
-if capitalize('hello') != 'Hello':
-    raise Exception('Function works wrong!')
-
-
-if capitalize('') != '':
-    raise Exception('Funcion works right!')
-
-print('All tests are done!')
+assert capitalize('') == ''
+assert capitalize('hello') == 'Hello'
+assert capitalize('hello') == 'hello'
 
